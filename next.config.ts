eb4d2025eeb92c2131ebf06f3a310c56920fddb1
horlans2020 <samhorlans@gmail.com>
2025-03-13 14:28:40 +0100

    more errors fixed

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  ignoreDuringBuilds: true,
-  "react/no-unescaped-entities": "off",
-  // swcMinify: true,
 };
 
 export default nextConfig;
